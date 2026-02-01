e949c134d3fa1331f8dc2c13638fb4c028aae506
jassperteacher-dev <jassperteacher@gmail.com>
2026-02-01 14:42:03 +0900

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,11 @@ with col_menu :
 def menu_button(category_name):
     current_menu = menu_data.get(category_name)
     #st.markdown()
-    st.write(current_menu)
+    #st.write(current_menu)
+
+    for name, price in current_menu.items():
+        st.write(name,price)
+
 
 with tab1: menu_button("식사류")
 with tab2: menu_button("안주류")
